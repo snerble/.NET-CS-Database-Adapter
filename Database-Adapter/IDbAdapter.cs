9882075b1f8d3d69d25c2b5fdf05f53f166fd033
Conor Groenenberg <0965749@hr.nl>
2020-02-29 13:18:33 +0100

    Moved solution and main project folder

---
+++ Database-Adapter/IDbAdapter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database
+{
+	/// <summary>
+	/// Represents a wrapper for communicating with an <see cref="IDbConnection"/>.
+	/// </summary>
+	public interface IDbAdapter
+	{
+		/// <summary>
+		/// Gets the <see cref="IDbConnection"/> instance used by this adapter.
+		/// </summary>
+		public abstract IDbConnection Connection { get; }
+	}
+}
