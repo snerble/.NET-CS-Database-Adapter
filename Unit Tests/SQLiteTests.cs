7ae35be04d9af53e26ceb8fee7c384d7cf23eca5
Spaalone_Babuguuscooties <0965749@hr.nl>
2020-02-29 20:55:45 +0100

    Added simple unit test

--- Unit Tests/SQLiteTests.cs
+++ Unit Tests/SQLiteTests.cs
@@ -1,11 +1,17 @@
-using Database.SQLite;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.IO;
 
-namespace UnitTesting
+namespace Database.SQLite
 {
 	[TestClass]
 	public class SQLiteTests
 	{
+		/// <summary>
+		/// The path to the database used by the tests in this class.
+		/// </summary>
+		public const string DatabaseName = "Database.db";
+
 		/// <summary>
 		/// Gets <see cref="SQLiteAdapter"/> used by all tests in this class.
 		/// </summary>
@@ -16,19 +22,38 @@ namespace UnitTesting
 		/// Initializes the resources shared by all tests in this class.
 		/// </summary>
 		[ClassInitialize]
-		public void ClassInit(TestContext _)
+		public static void ClassInit(TestContext _)
 		{
-			Database = new SQLiteAdapter("Unit Test Database.db");
+			// Remove database file if it exists
+			if (File.Exists(DatabaseName))
+			{
+				File.Delete(DatabaseName);
+				Trace.WriteLine("Deleted database at " + Path.GetFullPath(DatabaseName));
+			}
+			// Open a new connection
+			Database = new SQLiteAdapter(DatabaseName);
+			Trace.WriteLine($"Opened connection to {Database.Connection.FileName}");
 		}
 
 		/// <summary>
 		/// Disposes the resources used by all tests in this class.
 		/// </summary>
 		[ClassCleanup]
-		public void ClassCleanup()
+		public static void ClassCleanup()
 		{
 			Database.Dispose();
-		} 
+		}
 		#endregion
+
+		[TestMethod]
+		public void CreateTable()
+		{
+			var command = Database.Connection.CreateCommand();
+			command.CommandText = @"CREATE TABLE test (
+				id INTEGER PRIMARY KEY,
+				text_field STRING
+			);";
+			command.ExecuteNonQuery();
+		}
 	}
 }
