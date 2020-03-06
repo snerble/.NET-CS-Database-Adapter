a8a58dfe36f7cddac807fec6f45a1ff192211811
Conor Groenenberg <0965749@hr.nl>
2020-03-06 23:09:08 +0100

    Implemented SELECT query and a basic DELETE query

--- SQLite Adapter/SQLiteAdapter.cs
+++ SQLite Adapter/SQLiteAdapter.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Database.SQLite.Modeling;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SQLite;
 using System.Linq;
+using System.Text;
 
 namespace Database.SQLite
 {
@@ -21,26 +24,124 @@ namespace Database.SQLite
 			if (datasource is null) throw new ArgumentNullException(nameof(datasource));
 
 			Connection = new SQLiteConnection($"Data Source={datasource};foreign keys=true");
+
+			//Connection.AddTypeMapping("INTEGER", DbType.Byte, true);
+			//Connection.AddTypeMapping("INTEGER", DbType.SByte, true);
+			//Connection.AddTypeMapping("INTEGER", DbType.Int16, true);
+			//Connection.AddTypeMapping("INTEGER", DbType.UInt16, true);
+			//Connection.AddTypeMapping("INTEGER", DbType.Int32, true);
+			//Connection.AddTypeMapping("INTEGER", DbType.UInt32, true);
+			//Connection.AddTypeMapping("INTEGER", DbType.Int64, true);
+			//Connection.AddTypeMapping("INTEGER", DbType.UInt64, true);
+			//Connection.AddTypeMapping("REAL", DbType.Single, true);
+			//Connection.AddTypeMapping("REAL", DbType.Double, true);
+			//Connection.AddTypeMapping("NUMERIC", DbType.Decimal, true);
+			//Connection.AddTypeMapping("boolean", DbType.Boolean, true);
+			//Connection.AddTypeMapping("TEXT", DbType.String, true);
+			//Connection.AddTypeMapping("TEXT", DbType.StringFixedLength, true);
+			//Connection.AddTypeMapping("BLOB", DbType.Binary, true);
+
 			Connection.Open();
+		}
+
+		/// <summary>
+		/// Creates a new table that represents the given type <typeparamref name="T"/>.
+		/// </summary>
+		/// <typeparam name="T">The type to make a database table for.</typeparam>
+		public void CreateTable<T>() => CreateTable(typeof(T));
+		/// <summary>
+		/// Creates a new table that represents the given <paramref name="type"/>.
+		/// </summary>
+		/// <param name="type">The type to make a database table for.</param>
+		public void CreateTable(Type type)
+		{
+			var columns = Utils.GetProperties(type);
+
+			// Build the entire query
+			var sb = new StringBuilder("CREATE TABLE `");
+			sb.Append(type.Name);
+			sb.Append("` (\n");
+
+			// Build the columns
+			bool first = true;
+			foreach (var column in columns)
+			{
+				// Omit the comma for the first entry
+				if (!first)
+				{
+					sb.Append(",\n");
+					first = false;
+				}
+
+				sb.Append('\t');
+				// TODO: Implement overwritable column data
+				//var columnData = Utils.GetColumnData(column);
+				sb.Append(column.Name);
+				sb.Append(' ');
+				sb.Append(TypeMapping.GetType(column.PropertyType));
+
+				// Concatenate the column modifiers
+				foreach (var columnModifier in column.GetCustomAttributes(typeof(SQLiteColumnKeywordAttribute), false) as SQLiteColumnKeywordAttribute[])
+				{
+					sb.Append(' ');
+					sb.Append(columnModifier.Name);
+				}
+				first = false;
+			}
+			sb.Append("\n);");
+
+			// Create the command and execute it
+			using var command = new SQLiteCommand(Connection) { CommandText = sb.ToString() };
+			command.ExecuteNonQuery();
 		}
 
 		public int Delete<T>(string condition)
 		{
-			throw new NotImplementedException();
+			using var command = new SQLiteCommand(Connection) { CommandText = $"DELETE FROM `{Utils.GetTableName<T>()}` WHERE {condition}" };
+			return command.ExecuteNonQuery();
 		}
 		public int Delete<T>(T item)
 		{
-			throw new NotImplementedException();
+			var primary = Utils.GetProperties<T, PrimaryAttribute>().FirstOrDefault();
+			
+			// Create query that checks only for the primary key if one exists
+			if (primary != null)
+			{
+				using var command = Connection.CreateCommand();
+
+				var sb = new StringBuilder("DELETE FROM `");
+				sb.Append(Utils.GetTableName<T>());
+				sb.Append("` WHERE `");
+				// TODO: Implement overwritable column data
+				sb.Append(primary.Name);
+				sb.Append("` = @p");
+
+				// Add parameter for the primary column value
+				command.Parameters.Add(new SQLiteParameter("@p", primary.GetValue(item)));
+
+				// Execute the command and return the amount of affected rows
+				return command.ExecuteNonQuery();
+			}
+			// Otherwise make a query that compares the values of every column
+			else
+			{
+				throw new NotImplementedException("Deletion of an object without a primary key is currently not supported.");
+			}
 		}
 		public int Delete<T>(ICollection<T> items)
 		{
-			throw new NotImplementedException();
+			// TODO: Create one big query instead of many small ones
+			int affected_rows = 0;
+			// Delete each object individually and count all affected rows.
+			foreach (var item in items)
+				affected_rows += Delete(item);
+			return affected_rows;
 		}
 
 		public int Insert<T>(T item)
 		{
 			// Get all properties that will represent columns
-			var columns = Utils.GetAllColumns<T>().ToArray();
+			var columns = Utils.GetProperties<T>().ToArray();
 
 			using var command = Connection.CreateCommand();
 
@@ -53,6 +154,7 @@ namespace Database.SQLite
 			command.CommandText = $"INSERT INTO `{Utils.GetTableName<T>()}` ({fieldsText}) VALUES({parametersText}); ";
 
 			// Construct a parameter object for each value
+			// TODO: Add special case for enums
 			foreach (var property in columns)
 				command.Parameters.AddWithValue('@' + property.Name, property.GetValue(item) ?? DBNull.Value);
 
@@ -60,7 +162,8 @@ namespace Database.SQLite
 			command.ExecuteNonQuery();
 
 			// Retrieve the last inserted id with a new query
-			return (int)(long) new SQLiteCommand(Connection) { CommandText = "SELECT LAST_INSERT_ROWID()" }.ExecuteScalar();
+			using var scalarCommand = new SQLiteCommand(Connection) { CommandText = "SELECT LAST_INSERT_ROWID()" };
+			return (int)(long) scalarCommand.ExecuteScalar();
 		}
 		public int Insert<T>(ICollection<T> items)
 		{
@@ -68,16 +171,46 @@ namespace Database.SQLite
 			if (items.Count == 0) return -1;
 
 			// Get and return the first auto increment id and update the remaining items.
-			int scalar = Update(items.First());
+			int scalar = Insert(items.First());
 			foreach (var item in items.Skip(1))
-				Update(item);
+				Insert(item);
 			return scalar;
 		}
 
-		public IEnumerable<T> Select<T>() => Select<T>("1");
-		public IEnumerable<T> Select<T>(string condition)
+		public IEnumerable<T> Select<T>() where T : new() => Select<T>("1");
+		public IEnumerable<T> Select<T>(string condition) where T : new()
 		{
-			throw new NotImplementedException();
+			if (string.IsNullOrEmpty(condition))
+				throw new ArgumentException("Value may not be empty or null.", nameof(condition));
+
+			// Create and execute the command
+			using var command = new SQLiteCommand(Connection) { CommandText = $"SELECT * FROM `{Utils.GetTableName<T>()}` WHERE {condition}" };
+			using var reader = command.ExecuteReader();
+
+			var columns = Utils.GetProperties<T>();
+			while (reader.Read())
+			{
+				T outObj = new T();
+				for (int i = 0; i < reader.FieldCount; i++)
+				{
+					// TODO: Make the case-insensitivity optional
+					var column = columns.First(x => x.Name.ToLower() == reader.GetName(i).ToLower());
+					var type = column.PropertyType;
+					var value = reader.GetValue(i);
+
+					// Parse the value in case the model's type is an enum
+					if (type.IsEnum) value = Enum.Parse(type, value.ToString());
+
+					// Convert the value to the property type if it is a long (required since all int values are returned as a long)
+					else if (reader.GetFieldType(i).IsAssignableFrom(typeof(long)))
+						value = Convert.ChangeType(value, Nullable.GetUnderlyingType(type) ?? type);
+
+					// Set the value in outObj with the property
+					column.SetValue(outObj, value == DBNull.Value ? null : value);
+				}
+				// TODO: Add weak reference or some other system for accurately tracking the outObj
+				yield return outObj;
+			}
 		}
 
 		public int Update<T>(T item)
---
+++ SQLite Adapter/TypeMapping.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace Database.SQLite
+{
+	/// <summary>
+	/// Static class containing a mapping between .NET types and SQLite types.
+	/// </summary>
+	static class TypeMapping
+	{
+		///// <summary>
+		///// A <see cref="Dictionary{TKey, TValue}"/> that maps a <see cref="Type"/> to a <see cref="DbType"/>.
+		///// </summary>
+		//private static readonly Dictionary<Type, DbType> Mappings = new Dictionary<Type, DbType>
+		//{
+		//	[typeof(byte)] = DbType.Byte,
+		//	[typeof(sbyte)] = DbType.SByte,
+		//	[typeof(short)] = DbType.Int16,
+		//	[typeof(ushort)] = DbType.UInt16,
+		//	[typeof(int)] = DbType.Int32,
+		//	[typeof(uint)] = DbType.UInt32,
+		//	[typeof(long)] = DbType.Int64,
+		//	[typeof(ulong)] = DbType.UInt64,
+		//	[typeof(float)] = DbType.Single,
+		//	[typeof(double)] = DbType.Double,
+		//	[typeof(decimal)] = DbType.Decimal,
+		//	[typeof(bool)] = DbType.Boolean,
+		//	[typeof(string)] = DbType.String,
+		//	[typeof(char)] = DbType.StringFixedLength,
+		//	[typeof(Guid)] = DbType.Guid,
+		//	[typeof(DateTime)] = DbType.DateTime,
+		//	[typeof(DateTimeOffset)] = DbType.DateTimeOffset,
+		//	[typeof(byte[])] = DbType.Binary
+		//};
+		
+		/// <summary>
+		/// A <see cref="Dictionary{TKey, TValue}"/> that maps a <see cref="Type"/> to an SQLite type.
+		/// </summary>
+		private static readonly Dictionary<Type, string> Mappings = new Dictionary<Type, string>
+		{
+			[typeof(byte)] = "INTEGER",
+			[typeof(sbyte)] = "INTEGER",
+			[typeof(short)] = "INTEGER",
+			[typeof(ushort)] = "INTEGER",
+			[typeof(int)] = "INTEGER",
+			[typeof(uint)] = "INTEGER",
+			[typeof(long)] = "INTEGER",
+			[typeof(ulong)] = "INTEGER",
+			[typeof(float)] = "REAL",
+			[typeof(double)] = "REAL",
+			[typeof(decimal)] = "NUMERIC",
+			[typeof(bool)] = "boolean",
+			[typeof(string)] = "TEXT",
+			[typeof(char)] = "TEXT",
+			[typeof(Guid)] = "guid",
+			[typeof(DateTime)] = "datetime",
+			[typeof(DateTimeOffset)] = "datetimeoffset",
+			[typeof(TimeSpan)] = "time",
+			[typeof(byte[])] = "BLOB"
+		};
+
+		/// <summary>
+		/// Returns an equivalent SQLite type for the given type <typeparamref name="T"/>.
+		/// </summary>
+		/// <typeparam name="T">The type to get the equivalent SQLite type from.</typeparam>
+		public static string GetType<T>() => GetType(typeof(T));
+		/// <summary>
+		/// Returns an equivalent SQLite type for the given <paramref name="type"/>.
+		/// </summary>
+		/// <param name="type">The type to get the equivalent SQLite type from.</param>
+		/// <exception cref="KeyNotFoundException">Thrown when the given type does not have a mapping.</exception>
+		public static string GetType(Type type) => Mappings[Nullable.GetUnderlyingType(type) ?? type];
+
+		/// <summary>
+		/// Returns an equivalent <see cref="Type"/> for the given SQLite <paramref name="type"/>.
+		/// </summary>
+		/// <param name="type">The SQLite type to get the equivalent <see cref="Type"/> from.</param>
+		/// <exception cref="InvalidOperationException">Thrown when the given type does not have a mapping.</exception>
+		public static Type GetType(string type) => Mappings.First(x => x.Value == type).Key;
+	}
+}
